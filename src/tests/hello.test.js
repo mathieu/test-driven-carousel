5f39356c6b99d80166dc3e7235d2c72c3bc77f38
Mathieu <mathieu@marache.com>
2019-03-09 14:16:40 +0100

    :wrench: Initial jest setup

---
+++ src/tests/hello.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+describe('JSX', () => {
+  it('Calls React.createElement', () => {
+    const createElementSpy = jest.spyOn(React, 'createElement');
+    <h1>Hello, JSX!</h1>;
+    expect(createElementSpy).toHaveBeenCalledWith('h1', null, 'Hello, JSX!');
+  });
+});
