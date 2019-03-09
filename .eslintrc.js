704868aa5ca3934c3b7b8f788e3752bac81dd1b8
Mathieu <mathieu@marache.com>
2019-03-09 14:39:05 +0100

    :wrench: Added lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,11 @@ module.exports = {
   extends: ['eslint:recommended'],
   parserOptions: {
     ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+      modules: true,
+    },
   },
   env: {
     node: true,
--- src/tests/hello.test.js
+++ src/tests/hello.test.js
@@ -3,7 +3,7 @@ import React from 'react';
 describe('JSX', () => {
   it('Calls React.createElement', () => {
     const createElementSpy = jest.spyOn(React, 'createElement');
-    <h1>Hello, JSX!</h1>;
+    <ul>Hello, JSX!</ul>;
     expect(createElementSpy).toHaveBeenCalledWith('h1', null, 'Hello, JSX!');
   });
 });
