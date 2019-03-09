07377c81129b22aa9aeb4fca07c042dabbe613eb
Mathieu <mathieu@marache.com>
2019-03-09 19:52:12 +0100

    :wrench: Add Jest setup file

--- src/tests/CarouselButton.test.js
+++ src/tests/CarouselButton.test.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import { configure, shallow } from 'enzyme';
-import Adpater from 'enzyme-adapter-react-16';
+import { shallow } from 'enzyme';
 import CarouselButton from '../CarouselButton';
-
-configure({ adapter: new Adpater() });
 
 describe('CarouselButton', () => {
   const text = 'Button text';
