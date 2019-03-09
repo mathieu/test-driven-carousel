f01152c0841cf9c579a251a9e49295f5f88e49e4
Mathieu <mathieu@marache.com>
2019-03-09 14:48:26 +0100

    :wrench: Initial Wallaby setup

---
+++ wallaby.js
@@ -0,0 +1,16 @@
+module.exports = function(wallaby) {
+  return {
+    testFramework: 'jest',
+
+    env: {
+      type: 'node',
+    },
+
+    tests: ['src/tests/**/*.test.js'],
+    files: ['src/**/*.js', '!node_modules/**/*', '!**/*.test.js', '!**/.*'],
+
+    compilers: {
+      '**/*.js': wallaby.compilers.babel(),
+    },
+  };
+};
